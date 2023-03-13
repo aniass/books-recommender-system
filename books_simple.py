593a7b7ee3a34ce34d8912df699c337f4527e240
Aniass <a.staskiewicz4@gmail.com>
2023-03-13 17:09:29 +0100

    code refactor

--- books_simple.py
+++ books_simple.py
@@ -1,11 +1,13 @@
-'''The recommendation system with simplest recommendations method'''
+'''The simple recommendation system by Weighted Rating method'''
 
 import pandas as pd
 
-URL = 'C:\Python Scripts\Datasets\google_books\google_books_1299.csv'
+
+URL = '\Datasets\google_books\google_books_1299.csv'
 
 
 def clean_data(data):
+    '''Function to clean data'''
     data.drop(['Unnamed: 0'], axis=1, inplace=True)
     # removing duplicates
     df = data.drop_duplicates(keep=False)
@@ -20,21 +22,24 @@ def clean_data(data):
     
 
 def read_data(path):
+    '''Read and preprocess data'''
     data = pd.read_csv(path)
     df = clean_data(data)
     return df
 
 
 def simple_recommend(df):
+    """Function to calculate a simple recommendations based on Weighted Rating"""
     C = df['rating'].mean()
     m = df['voters'].quantile(0.90)
     V = df['voters']
     R = df['rating']
     # filtering out all qualified books into a new DataFrame
     q_book = df.copy().loc[df['voters'] >= m]
+    # calculating the weighted rating 
     q_book['score'] = (V/(V+m) * R) + (m/(m+V) * C)
     q_book = q_book.sort_values('score', ascending=False)
-    # the top 15 movies
+    # the top 15 books
     print(q_book[['title', 'voters', 'rating', 'score']].head(10))
 
 
