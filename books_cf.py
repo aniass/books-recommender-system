df9e570c17591358f1ae2db64c8ef5b643573a38
Aniass <a.staskiewicz4@gmail.com>
2023-05-11 18:10:13 +0200

    adding file

---
+++ books_cf.py
@@ -0,0 +1,104 @@
+'''The recommendation system with Collaborative Filtering by KNN model'''
+
+import pandas as pd
+from sklearn import neighbors
+from sklearn.preprocessing import MinMaxScaler
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+
+URL = 'C:\\Python Scripts\\Datasets\\goodreads book\\books.csv'
+
+
+def clean_data(data):
+    data['language_code'].replace(to_replace=['eng', 'en-US', 'en-GB', 'enm', 'en-CA',
+                                        'fre', 'spa', 'mul', 'grc','ger',
+                                        'jpn', 'ara', 'nl', 'zho','lat', 'por','srp',
+                                        'ita', 'rus', 'msa', 'glg', 'wel', 'swe', 
+                                        'nor', 'tur','gla', 'ale'],
+                                 value=['English', 'English', 'English', 'English', 'English',
+                                        'French','Spanish','Multiple language',
+                                        'Greek','German','Japanese','Arabic',
+                                        'Dutch','Chinese','Latvian','Portuguese',
+                                        'Serbian','Initial teaching language',
+                                        'Russian','Modern Standard Arabic',
+                                        'Galician','Welsh','Swedish','Murik',
+                                        'Turkish','Gaelic','Afro-Asiatic'], inplace=True)
+    return data
+
+
+def read_data(path):
+    data = pd.read_csv(path, error_bad_lines=False)
+    df = clean_data(data) 
+    return df
+
+
+def create_rating(row):
+    """The function to create a column rating between"""
+    if row >= 0 and row <=1:
+        return '0-1'
+    if row >= 1 and row <=2:
+        return '1-2'
+    if row >= 2 and row <=3:
+        return '2-3'
+    if row >= 3 and row <=4:
+        return '3-4'
+    if row >= 4 and row <=5:
+        return '4-5'
+
+
+def preprocess_data(df):
+    df['rating_between'] = df['average_rating'].apply(create_rating)
+    rating_df = pd.get_dummies(df['rating_between'])
+    language_df = pd.get_dummies(df['language_code'])
+    features = pd.concat([rating_df, 
+                      language_df, 
+                      df['average_rating'], 
+                      df['ratings_count']], axis=1)
+    return features
+
+
+def get_model(features):
+    scaler = MinMaxScaler()
+    feature = scaler.fit_transform(features)
+    model = neighbors.NearestNeighbors(n_neighbors=6, algorithm='auto')
+    model.fit(feature)
+    distance, indices = model.kneighbors(feature)
+    return distance, indices 
+
+
+def make_recommendations(name):
+    """The function to get recommendations"""
+    df = read_data(URL)
+    features = preprocess_data(df)
+    distance, indices = get_model(features)
+    book_list = []
+    book_id = df[df['title'] == name].index
+    book_id = book_id[0]
+    for newid in indices[book_id]:
+        book_list.append(df.loc[newid].title)
+    print("Recommended books are:\n")
+    for i in range(0,len(book_list)):
+        print(f"{i+1}){book_list[i]}")
+    
+
+def author_recommendations(author):
+    """The function to get recommendations by the author"""
+    df = read_data(URL)
+    features = preprocess_data(df)
+    distance, indices = get_model(features)
+    author_list = []
+    books=[]
+    author_id = df[df['authors'] == author].index
+    author_id = author_id[0]
+    for newid in indices[author_id]:
+        author_list.append(df.loc[newid].authors)
+        books.append(df.loc[newid].title)
+    print("Based the author recommended books are:\n")    
+    for i in range(0,len(author_list)):
+      print(f"{i+1})Author:{author_list[i]}, '{books[i]}'")
+
+
+if __name__ == '__main__':
+    make_recommendations("Jane Eyre")
+    author_recommendations("Dan Brown")
---
+++ books_matrix.py
@@ -0,0 +1,58 @@
+'''The recommendation system with matrix factorization method'''
+
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import TruncatedSVD
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+
+URL = 'C:\\Python Scripts\\Datasets\\goodreads book\\books.csv'
+
+popularity_threshold = 1000
+
+
+def read_data(path):
+    df = pd.read_csv(path, error_bad_lines=False)
+    return df
+
+
+def preparing_data():
+    df = read_data(URL)
+    id_rating = df[['bookID', 'average_rating', 'title', 'ratings_count']]
+    # choose books which received above 1000 ratings
+    id_rating2 = id_rating.query('ratings_count >= @popularity_threshold')
+    # convert the table into a 2D matrix
+    id_rating_pivot = id_rating2.pivot(index = 'bookID', columns = 'title', values = 'average_rating').fillna(0)
+    return id_rating_pivot
+
+
+def dimensionality_reduction(id_rating_pivot):
+    # transpose the matrix 
+    X = id_rating_pivot.values.T
+    # SVD model
+    SVD = TruncatedSVD(n_components=12, random_state=17)
+    matrix = SVD.fit_transform(X)
+    # calculating the Pearson’s R correlation coefficient 
+    corr = np.corrcoef(matrix)
+    return corr
+    
+
+def matrix_factorization(name):
+    """The recommendations for chosen book. 
+    The function allows to find the books that have high correlation coefficients
+    (between 0.9 and 1.0) with chosen book"""
+    id_rating_pivot = preparing_data()
+    corr = dimensionality_reduction(id_rating_pivot)
+    book_title = id_rating_pivot.columns
+    book_list = list(book_title)
+    book = book_list.index(name)
+    corr_book = corr[book]
+    print("Recommended books are:\n")
+    print(list(book_title[(corr_book < 1.0) & (corr_book > 0.9)]))
+    
+    
+if __name__ == '__main__':
+    matrix_factorization("Animal Farm")
+    matrix_factorization("The Da Vinci Code")
+    
